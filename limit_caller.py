27c1ae745d2e27774b705c2a80de94d3fc746ec5
chazovtema <chazovtema@mail.ru>
2024-01-11 22:40:36 +0500

    Добавил автоудаление ограничителя для чата через 60 сек

--- limit_caller.py
+++ limit_caller.py
@@ -1,24 +1,35 @@
-from datetime import datetime
+from time import time
 from typing import Coroutine
+from asyncio import Task
+import asyncio
 
 from limiter import Limiter
-
 
 class LimitCaller():
     
     def __init__(self) -> None:
         self.main_limiter = Limiter(30)
-        self.chats: dict[int, Limiter] = {}
-
+        self.chats: dict[int, tuple[Limiter | float | Task]] = {}
+        
+    async def delete_task(self, chat: int):
+        
+        await asyncio.sleep(60)
+        del self.chats[chat]
+        
     
     async def call(self, chat: int, coro: Coroutine):
         
         async with self.main_limiter:
-            limiter = self.chats.get(chat)
-            if not limiter:
+            limiter_list = self.chats.get(chat)
+            
+            if not limiter_list:
                 limiter = Limiter(3, 10)
-                self.chats[chat] = limiter
-                
-            async with limiter:
-                
-                return await coro
+                task = asyncio.create_task(self.delete_task(chat))
+                self.chats[chat] = (limiter, time(), task)
+            else:
+                task.cancel()
+                limiter_list[1] = time()
+                limiter_list[2] = asyncio.create_task(self.delete_task(chat))
+                async with limiter:
+                    
+                    return await coro
