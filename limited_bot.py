8bb0a4f6a600ae02b076d24a0ee454e24977fa99
chazovtema <chazovtema@mail.ru>
2024-01-13 14:47:37 +0500

    Исправил патчинг оригинального бота

--- limited_bot.py
+++ limited_bot.py
@@ -8,36 +8,21 @@ from aiogram import Bot
 
 from limit_caller import LimitCaller
 
-print(Bot.__call__)
 T = TypeVar("T")
-# ORIGINAL_CALL = FunctionType(Bot.__call__, '__call__')
-ORIGINAL_CALL = copy(Bot.__call__)
-
 
 class LimitedBot(Bot):
     
-    def __init__(self, token: str, session: BaseSession | None = None, parse_mode: str | None = None, disable_web_page_preview: bool | None = None, protect_content: bool | None = None) -> None:
-        super().__init__(token, session, parse_mode, disable_web_page_preview, protect_content)
-        self.caller = LimitCaller()
-    
-    @wraps(ORIGINAL_CALL)
     async def __call__(self, method: TelegramMethod[T], request_timeout: Optional[int] = None):
-        print(ORIGINAL_CALL)
-        coro = ORIGINAL_CALL(method, request_timeout)
-        return await ORIGINAL_CALL
+        if not hasattr(self, 'caller'):
+             self.caller = LimitCaller()
+        coro = self.session(self, method, timeout=request_timeout)
         if hasattr(method, 'chat_id'):
             return await self.caller.call(method.chat_id, coro)
         else:
             return await coro
 
-
 def path_bot():
     
-    """Патчит бота в aiogram, эти изменения не обратимы"""
+    """Patches the bot, these changes are not reversible"""
     
-    fn = LimitedBot.__call__
-    Bot.__call__ = fn
-    print(ORIGINAL_CALL)
-    print(Bot.__call__)
-    print(LimitCaller.__call__)
-    
+    Bot.__call__ = LimitedBot.__call__
